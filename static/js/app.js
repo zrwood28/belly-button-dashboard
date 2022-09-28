d49ed9ba5accdb6943f620b4191e038091611b7c
Zach Wood <zachrwood28@gmail.com>
2022-09-27 21:33:38 -0500

    Updated bar graph function.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,40 @@
 // Thanks to Dom for much of this code; I attended his tutorial session.
+
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
 function drawBargraph(sampleId) {
     console.log(`drawBargraph(${sampleId})`);
+    
+    d3.json(url).then(data => {
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        let barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        };
+
+        let barArray = [barData];
+
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    });
 }
 
 function demoData(sampleId) {
@@ -30,8 +64,6 @@ function InitDashboard() {
 
     let selector = d3.select("#selDataset");
 
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
     d3.json(url).then(data => {
         console.log(data)
 
@@ -53,10 +85,6 @@ function InitDashboard() {
         demoData(initialId);
 
         drawGauge(initialId);
-
-        optionChanged
-
-
 
     });
 }
