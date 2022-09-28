0f4dcb099670c4579db237dfa716e30b11ba394b
Zach Wood <zachrwood28@gmail.com>
2022-09-27 21:43:43 -0500

    Added bubble chart.

--- static/js/app.js
+++ static/js/app.js
@@ -43,6 +43,41 @@ function demoData(sampleId) {
 
 function drawBubbleGraph(sampleId) {
     console.log(`drawBubbleGraph(${sampleId})`);
+
+    d3.json(url).then(data => {
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+
+        let bubbleArray = [bubbleData];
+
+        let bubbleLayout = {
+            title: "Bacteria Cultures per Sample",
+            margin: {t: 30},
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout);
+
+    });
 }
 
 function drawGauge(sampleId) {
